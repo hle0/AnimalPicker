9338cd175e76c5e956ffedacd1657a21f1f95894
hle0 <91701075+hle0@users.noreply.github.com>
2022-02-22 19:59:47 -0500

    add error message when an invalid option is chosen

--- picker.py
+++ picker.py
@@ -9,6 +9,9 @@ def main():
     choice = input('What animal you like to see?\n').lower()
     if choice in ascii_art.keys():
         print(ascii_art[choice])
+    else:
+        print('Sorry, that\'s not a valid option!')
+        print(f'Valid options are {", ".join(ascii_art.keys())}')
 
 if __name__ == '__main__':
     main()
